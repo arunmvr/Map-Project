508ffa9714e6d5c3b5216047381fd1ccd857c71a
Arun <arun4310@gmail.com>
2016-04-20 13:19:14 +0530

    adding files

--- src/com/mapproject/MapActivity.java
+++ src/com/mapproject/MapActivity.java
@@ -108,12 +108,11 @@ public class MapActivity extends FragmentActivity implements LocationListener,
 					googlemap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                             new LatLng(location.getLatitude(), location.getLongitude()), 13));
 					
-					CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
-                    .zoom(15)                   // Sets the zoom
-                    .bearing(90)                // Sets the orientation of the camera to east
-                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
-                    .build();                   // Creates a CameraPosition from the builder
+					CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                                                                      .zoom(15)                   // Sets the zoom
+                                                                      .bearing(90)                // Sets the orientation of the camera to east
+                                                                      .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                                                                      .build();                   // Creates a CameraPosition from the builder
 					googlemap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 					
 				}
