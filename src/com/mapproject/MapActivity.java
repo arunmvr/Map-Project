6d17c23ef3ca437d534a72657a4cf2755a2e29f2
Arun <arun4310@gmail.com>
2016-04-20 13:16:43 +0530

    adding files

--- src/com/mapproject/MapActivity.java
+++ src/com/mapproject/MapActivity.java
@@ -33,10 +33,10 @@ public class MapActivity extends FragmentActivity implements LocationListener,
 		setContentView(R.layout.mapmain);
 
 		Intent it = this.getIntent();
-		latb = it.getDoubleExtra("latb", 12.9716); // Latitude For Bangalore
-		lngb = it.getDoubleExtra("lngb", 77.5946); // Longitude for Bangalore
-		latc = it.getDoubleExtra("latc", 13.0827); // Latitude for Chennai
-		lngc = it.getDoubleExtra("lngc", 80.2707); // Longitude for Chennai
+		latb = it.getDoubleExtra("latb", 12.9716);  //Latitude For Bangalore
+		lngb = it.getDoubleExtra("lngb", 77.5946);  //Longitude for Bangalore
+		latc = it.getDoubleExtra("latc", 13.0827);  //Latitude for Chennai
+		lngc = it.getDoubleExtra("lngc", 80.2707);  //Longitude for Chennai
 		LatLng Bangalore = new LatLng(latb, lngb);
 		LatLng Chennai = new LatLng(latc, lngc);
 		choice = it.getIntExtra("Choice", 0);
@@ -53,48 +53,27 @@ public class MapActivity extends FragmentActivity implements LocationListener,
 			} else {
 				SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager()
 						.findFragmentById(R.id.map);
-
+				
 				LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
+				
 				googlemap = fm.getMap();
-				googlemap.setMyLocationEnabled(true); // Set true to get current
-														// location
-				googlemap.getUiSettings().setZoomControlsEnabled(true); // Set
-																		// true
-																		// to
-																		// display
-																		// Zoom
-																		// in/Zoom
-																		// out
-																		// Button
-				googlemap.getUiSettings().setCompassEnabled(true); // Set true
-																	// to
-																	// display
-																	// Compass
+				googlemap.setMyLocationEnabled(true);  //Set true to get current location
+				googlemap.getUiSettings().setZoomControlsEnabled(true); //Set true to display Zoom in/Zoom out Button 
+				googlemap.getUiSettings().setCompassEnabled(true); //Set true to display Compass
+
+				
 
 				boolean gps_enabled = false;
 				boolean network_enabled = false;
 				try {
 					gps_enabled = locationManager
-							.isProviderEnabled(LocationManager.GPS_PROVIDER); // Check
-																				// if
-																				// location
-																				// is
-																				// enabled
-																				// in
-																				// device
+							.isProviderEnabled(LocationManager.GPS_PROVIDER); // Check if location is enabled in device
 				} catch (Exception ex) {
 				}
 
 				try {
 					network_enabled = locationManager
-							.isProviderEnabled(LocationManager.NETWORK_PROVIDER); // Check
-																					// if
-																					// network
-																					// is
-																					// enabled
-																					// in
-																					// device
+							.isProviderEnabled(LocationManager.NETWORK_PROVIDER); //Check if network is enabled in device
 				} catch (Exception ex) {
 				}
 
@@ -114,36 +93,29 @@ public class MapActivity extends FragmentActivity implements LocationListener,
 						true);
 				Location location = locationManager
 						.getLastKnownLocation(provider);
-
+				
 				if (location != null) {
 					onLocationChanged(location);
-					locationManager.requestLocationUpdates(provider, 20, 0,
-							this);
-
-					// double latitude = location.getLatitude();
-					// double longitude = location.getLongitude();
-					// LatLng latLng = new LatLng(latitude, longitude);
-					// googlemap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-					// googlemap.animateCamera(CameraUpdateFactory.zoomTo(15));
-
+					locationManager.requestLocationUpdates(provider, 20, 0, this);
+					
+					
+					//double latitude = location.getLatitude();
+					//double longitude = location.getLongitude();
+					//LatLng latLng = new LatLng(latitude, longitude);
+					//googlemap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+					//googlemap.animateCamera(CameraUpdateFactory.zoomTo(15));
+					
 					googlemap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-							new LatLng(location.getLatitude(), location
-									.getLongitude()), 13));
-
+                            new LatLng(location.getLatitude(), location.getLongitude()), 13));
+					
 					CameraPosition cameraPosition = new CameraPosition.Builder()
-							.target(new LatLng(location.getLatitude(), location
-									.getLongitude())) // Sets the center of the
-														// map to location user
-							.zoom(15) // Sets the zoom
-							.bearing(90) // Sets the orientation of the camera
-											// to east
-							.tilt(40) // Sets the tilt of the camera to 30
-										// degrees
-							.build(); // Creates a CameraPosition from the
-										// builder
-					googlemap.animateCamera(CameraUpdateFactory
-							.newCameraPosition(cameraPosition));
-
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(15)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+					googlemap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+					
 				}
 			}
 
@@ -173,25 +145,21 @@ public class MapActivity extends FragmentActivity implements LocationListener,
 	@Override
 	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
-		// double latitude = location.getLatitude();
-		// double longitude = location.getLongitude();
-		// LatLng latLng = new LatLng(latitude, longitude);
-		// googlemap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-		// googlemap.animateCamera(CameraUpdateFactory.zoomTo(15));
-
-		googlemap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
-				location.getLatitude(), location.getLongitude()), 13));
-
-		CameraPosition cameraPosition = new CameraPosition.Builder()
-				.target(new LatLng(location.getLatitude(), location
-						.getLongitude())) // Sets the center of the map to
-											// location user
-				.zoom(17) // Sets the zoom
-				.bearing(90) // Sets the orientation of the camera to east
-				.tilt(40) // Sets the tilt of the camera to 30 degrees
-				.build(); // Creates a CameraPosition from the builder
-		googlemap.animateCamera(CameraUpdateFactory
-				.newCameraPosition(cameraPosition));
+		//double latitude = location.getLatitude();
+		//double longitude = location.getLongitude();
+		//LatLng latLng = new LatLng(latitude, longitude);
+		//googlemap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+		//googlemap.animateCamera(CameraUpdateFactory.zoomTo(15));
+		
+		googlemap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(location.getLatitude(), location.getLongitude()), 13));
+		
+		CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                                        .zoom(17)                   // Sets the zoom
+                                        .bearing(90)                // Sets the orientation of the camera to east
+                                        .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                                        .build();                   // Creates a CameraPosition from the builder
+		googlemap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
 	}
 
